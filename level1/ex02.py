6236b9584e864ed8a2ec69f2c25d357ed9aad6b1
TaeHyun Kim <57697593+git-kth@users.noreply.github.com>
2020-07-12 19:29:45 +0900

    level1

---
+++ level1/ex02.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    a = a - 1
+    b = b - 1
+    month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    s = sum(month[:a]) + b
+    answer = day[s % 7]
+    return answer
